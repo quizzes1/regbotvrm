bdc53d14595b3e6fb9b864408c779fabb551fa59
Pavel Putintsev <lomaakarart@mail.ru>
2024-09-06 00:56:43 +0300

    sqltest

--- app/keyboards.py
+++ app/keyboards.py
@@ -1,13 +1,27 @@
 from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup)
+from app.requests import get_categories, get_item_category
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 main = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text='hey')], 
-    [KeyboardButton(text='idi nahooy'), KeyboardButton(text='zhopa')]
+    [KeyboardButton(text='Catalogue')], 
+    [KeyboardButton(text='Contacts')],
+    [KeyboardButton(text='Something')]
 ],
                            resize_keyboard=True,
                            input_field_placeholder='choose the point of the menu')
 
-settings = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text='settings', callback_data='settings')],
-    [InlineKeyboardButton(text='check your mother', callback_data='checkmom')]
-])
+async def categories():
+    all_categories = await get_categories()
+    keyboard = InlineKeyboardBuilder()
+    for category in all_categories:
+        keyboard.add(InlineKeyboardButton(text=category.name, callback_data=f'category_{category.id}'))
+    keyboard.add(InlineKeyboardButton(text='Main menu', callback_data='to_main'))
+    return keyboard.adjust(2).as_markup()
+
+async def items(category_id):
+    all_items = await get_item_category(category_id)
+    keyboard = InlineKeyboardBuilder()
+    for item in all_items:
+        keyboard.add(InlineKeyboardButton(text=item.name, callback_data=f'item_{item.id}'))
+    keyboard.add(InlineKeyboardButton(text='Main menu', callback_data='to_main'))
+    return keyboard.adjust(2).as_markup()
--- run.py
+++ run.py
@@ -4,17 +4,20 @@ import logging
 from aiogram import Bot, Dispatcher
 from app.handlers import router
 from config import TOKEN
+from app.module import async_main
 
 bot = Bot(token = TOKEN)
 dp = Dispatcher()
 
 
 async def main():
+    await async_main()
     dp.include_router(router)
     await dp.start_polling(bot)
 
 
 if __name__ == '__main__':
+    
     logging.basicConfig(level=logging.INFO)
     try:
         asyncio.run(main())
